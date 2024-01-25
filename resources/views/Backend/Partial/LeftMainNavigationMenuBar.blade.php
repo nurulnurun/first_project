bccc8ec288848e298aa2907b94b91f731f46042b
Nurul Haque <nurulhaque.engineer@gmail.com>
2024-01-25 12:27:46 +0600

    Left Main Navigation MenuBar setup

--- resources/views/Backend/Partial/LeftMainNavigationMenuBar.blade.php
+++ resources/views/Backend/Partial/LeftMainNavigationMenuBar.blade.php
@@ -1,126 +1,75 @@
 <ul class="metismenu" id="menu">
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-home"></i>
-							<span class="nav-text">Dashboard</span>
+							<span class="nav-text">Category</span>
 						</a>
                         <ul aria-expanded="false">
-							<li><a href="index.html">Dashboard Light</a></li>
-							<li><a href="index-2.html">Dashboard Dark</a></li>
-							<li><a href="kanban.html">Kanban</a></li>
-							<li><a href="clients.html">Clients</a></li>
-							<li><a href="project-details.html">Project Details</a></li>
-							<li><a href="message.html">Messages</a></li>
-							<li><a href="latest-activity.html">Latest Activity</a></li>
+							<li><a href="index.html">Add new Category</a></li>
+							<li><a href="index-2.html">Category List</a></li>
 						</ul>
 
                     </li>
 
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 						<i class="fas fa-info-circle"></i>
-							<span class="nav-text">Apps</span>
+							<span class="nav-text">User</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="./app-profile.html">Profile</a></li>
-							<li><a href="./post-details.html">Post Details</a></li>
-                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Email</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="./email-compose.html">Compose</a></li>
-                                    <li><a href="./email-inbox.html">Inbox</a></li>
-                                    <li><a href="./email-read.html">Read</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./app-calender.html">Calendar</a></li>
-							<li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Shop</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="./ecom-product-grid.html">Product Grid</a></li>
-									<li><a href="./ecom-product-list.html">Product List</a></li>
-									<li><a href="./ecom-product-detail.html">Product Details</a></li>
-									<li><a href="./ecom-product-order.html">Order</a></li>
-									<li><a href="./ecom-checkout.html">Checkout</a></li>
-									<li><a href="./ecom-invoice.html">Invoice</a></li>
-									<li><a href="./ecom-customers.html">Customers</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="./app-profile.html">Add new User</a></li>
+							<li><a href="./post-details.html">User List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-chart-line"></i>
-							<span class="nav-text">Charts</span>
+							<span class="nav-text">Customer</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="./chart-flot.html">Flot</a></li>
-                            <li><a href="./chart-morris.html">Morris</a></li>
-                            <li><a href="./chart-chartjs.html">Chartjs</a></li>
-                            <li><a href="./chart-chartist.html">Chartist</a></li>
-                            <li><a href="./chart-sparkline.html">Sparkline</a></li>
-                            <li><a href="./chart-peity.html">Peity</a></li>
+                            <li><a href="./chart-flot.html">Add new Customer</a></li>
+                            <li><a href="./chart-morris.html">Customer List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fab fa-bootstrap"></i>
-							<span class="nav-text">Bootstrap</span>
+							<span class="nav-text">Product</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="./ui-accordion.html">Accordion</a></li>
-                            <li><a href="./ui-alert.html">Alert</a></li>
-                            <li><a href="./ui-badge.html">Badge</a></li>
-                            <li><a href="./ui-button.html">Button</a></li>
-                            <li><a href="./ui-modal.html">Modal</a></li>
-                            <li><a href="./ui-button-group.html">Button Group</a></li>
-                            <li><a href="./ui-list-group.html">List Group</a></li>
-                            <li><a href="./ui-card.html">Cards</a></li>
-                            <li><a href="./ui-carousel.html">Carousel</a></li>
-                            <li><a href="./ui-dropdown.html">Dropdown</a></li>
-                            <li><a href="./ui-popover.html">Popover</a></li>
-                            <li><a href="./ui-progressbar.html">Progressbar</a></li>
-                            <li><a href="./ui-tab.html">Tab</a></li>
-                            <li><a href="./ui-typography.html">Typography</a></li>
-                            <li><a href="./ui-pagination.html">Pagination</a></li>
-                            <li><a href="./ui-grid.html">Grid</a></li>
-
+                            <li><a href="./ui-accordion.html">Add new Product</a></li>
+                            <li><a href="./ui-alert.html">Product List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-heart"></i>
-							<span class="nav-text">Plugins</span>
+							<span class="nav-text">Brand</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="./uc-select2.html">Select 2</a></li>
-                            <li><a href="./uc-nestable.html">Nestedable</a></li>
-                            <li><a href="./uc-noui-slider.html">Noui Slider</a></li>
-                            <li><a href="./uc-sweetalert.html">Sweet Alert</a></li>
-                            <li><a href="./uc-toastr.html">Toastr</a></li>
-                            <li><a href="./map-jqvmap.html">Jqv Map</a></li>
-							<li><a href="./uc-lightgallery.html">Light Gallery</a></li>
+                            <li><a href="./uc-select2.html">Add new Brand</a></li>
+                            <li><a href="./uc-nestable.html">Brand List</a></li>
                         </ul>
                     </li>
-                    <li><a href="widget-basic.html" class="" aria-expanded="false">
+                    <!-- <li><a href="widget-basic.html" class="" aria-expanded="false">
 							<i class="fas fa-user-check"></i>
-							<span class="nav-text">Widget</span>
+							<span class="nav-text"></span>
 						</a>
-					</li>
+					</li> -->
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-file-alt"></i>
-							<span class="nav-text">Forms</span>
+							<span class="nav-text">Order</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="./form-element.html">Form Elements</a></li>
-                            <li><a href="./form-wizard.html">Wizard</a></li>
-                            <li><a href="./form-ckeditor.html">CkEditor</a></li>
-                            <li><a href="form-pickers.html">Pickers</a></li>
-                            <li><a href="form-validation.html">Form Validate</a></li>
+                            <li><a href="./form-element.html">Add new Order</a></li>
+                            <li><a href="./form-wizard.html">Order List</a></li>
                         </ul>
                     </li>
                     <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-table"></i>
-							<span class="nav-text">Table</span>
+							<span class="nav-text">Maintenance</span>
 						</a>
                         <ul aria-expanded="false">
-                            <li><a href="table-bootstrap-basic.html">Bootstrap</a></li>
-                            <li><a href="table-datatable-basic.html">Datatable</a></li>
+                            <li><a href="table-bootstrap-basic.html">Utility</a></li>
+                            <li><a href="table-datatable-basic.html">Break Down</a></li>
                         </ul>
                     </li>
-                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+                    <!-- <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
 							<i class="fas fa-clone"></i>
 							<span class="nav-text">Pages</span>
 						</a>
@@ -139,5 +88,5 @@
                             <li><a href="./page-lock-screen.html">Lock Screen</a></li>
                             <li><a href="./empty-page.html">Empty Page</a></li>
                         </ul>
-                    </li>
+                    </li> -->
                 </ul>
